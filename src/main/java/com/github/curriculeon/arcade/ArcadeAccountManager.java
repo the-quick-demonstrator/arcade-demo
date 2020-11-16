a970f9e41620c701b5209c5cd3bd1f5f0a4998ef
git-leon <xleonhunter@gmail.com>
2020-11-16 17:53:21 -0500

    updated exceptions

--- src/main/java/com/github/curriculeon/arcade/ArcadeAccountManager.java
+++ src/main/java/com/github/curriculeon/arcade/ArcadeAccountManager.java
@@ -7,28 +7,40 @@ package com.github.curriculeon.arcade;
  */
 public class ArcadeAccountManager {
     /**
-     * @param accountName name of account to be returned
+     * @param accountName     name of account to be returned
      * @param accountPassword password of account to be returned
      * @return `ArcadeAccount` with specified `accountName` and `accountPassword`
      */
     public ArcadeAccount getAccount(String accountName, String accountPassword) {
-        return null;
+        String currentMethodName = new Object(){}.getClass().getEnclosingMethod().getName();
+        String currentClassName = getClass().getName();
+        String errorMessage = "Method with name [ %s ], defined in class with name [ %s ] has  not yet been implemented";
+        throw new RuntimeException(String.format(errorMessage, currentMethodName, currentClassName));
     }
 
     /**
      * logs & creates a new `ArcadeAccount`
-     * @param accountName name of account to be created
+     *
+     * @param accountName     name of account to be created
      * @param accountPassword password of account to be created
      * @return new instance of `ArcadeAccount` with specified `accountName` and `accountPassword`
      */
     public ArcadeAccount createAccount(String accountName, String accountPassword) {
-        return null;
+        String currentMethodName = new Object(){}.getClass().getEnclosingMethod().getName();
+        String currentClassName = getClass().getName();
+        String errorMessage = "Method with name [ %s ], defined in class with name [ %s ] has  not yet been implemented";
+        throw new RuntimeException(String.format(errorMessage, currentMethodName, currentClassName));
     }
 
     /**
      * logs & registers a new `ArcadeAccount` to `this.getArcadeAccountList()`
+     *
      * @param arcadeAccount the arcadeAccount to be added to `this.getArcadeAccountList()`
      */
     public void registerAccount(ArcadeAccount arcadeAccount) {
+        String currentMethodName = new Object(){}.getClass().getEnclosingMethod().getName();
+        String currentClassName = getClass().getName();
+        String errorMessage = "Method with name [ %s ], defined in class with name [ %s ] has  not yet been implemented";
+        throw new RuntimeException(String.format(errorMessage, currentMethodName, currentClassName));
     }
 }
